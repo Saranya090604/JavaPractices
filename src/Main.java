1c6dbb867823419ec6340ac04587829c286a8b11
saranya <saranyasanjana09@gmail.com>
2026-08-07 19:43:10 +0530

    pattern3-program

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print("* ");
+            for(int j=1; j<=n; j++){
+                System.out.print(i + " ");
             }
             System.out.println();
         }
